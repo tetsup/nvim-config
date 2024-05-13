8234b02e4fd538339c39172138ec1227d424754d
tetsup <58599705+tetsup@users.noreply.github.com>
2024-05-13 12:33:12 +0900

    add python path

--- lua/config/denops.lua
+++ lua/config/denops.lua
@@ -7,7 +7,9 @@ end
 
 if vim.fn['has("win32")'] then
   vim.g.denops_deno = execute_cmd('where deno')
+  vim.g.python3_host_prog = execute_cmd('where python3')
 else
   vim.g.denops_deno = execute_cmd('which deno')
+  vim.g.python3_host_prog = execute_cmd('which python3')
 end
 
