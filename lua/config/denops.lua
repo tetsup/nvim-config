c354493e275438839bcc9c8eb4de6496637ff80e
tetsup <58599705+tetsup@users.noreply.github.com>
2024-05-15 07:24:54 +0900

    windowsのパス修正

--- lua/config/denops.lua
+++ lua/config/denops.lua
@@ -6,10 +6,13 @@ local execute_cmd = function(cmd)
 end
 
 if vim.fn['has("win32")'] then
-  vim.g.denops_deno = execute_cmd('where deno')
-  vim.g.python3_host_prog = execute_cmd('where python3')
+  vim.g.denops_deno = execute_cmd(
+    'for %%I in (deno.exe) do if exist %%~$path:I echo %%~$path:I'
+  )
+  vim.g.python3_host_prog = execute_cmd(
+    'for %%I in (python3.exe) do if exist %%~$path:I echo %%~$path:I'
+  )
 else
   vim.g.denops_deno = execute_cmd('which deno')
   vim.g.python3_host_prog = execute_cmd('which python3')
 end
-
